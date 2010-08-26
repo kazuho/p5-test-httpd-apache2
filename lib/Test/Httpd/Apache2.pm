8cf3ae307d74cfad9b1ec231d4371f3308f54f8c
Kazuho Oku <kazuhooku@gmail.com>
2010-08-26 17:36:42 +0900

    write POD, use readme_from

--- lib/Test/Httpd/Apache2.pm
+++ lib/Test/Httpd/Apache2.pm
@@ -117,3 +117,71 @@ sub conf_file {
 1;
 
 __END__
+
+=head1 NAME
+
+Test::Httpd::Apache2 - Apache2 runner for tests
+
+=head1 SYNOPSIS
+
+    use Test::Httpd::Apache2;
+
+    my $httpd = Test::Httpd::Apache2->new(
+        custom_conf => << 'EOT',
+    DocumentRoot "htdocs"
+    EOT
+    );
+
+    # do whatever you want
+    my $url = "http://" . $httpd->listen . "/";
+    ...
+
+=head1 DESCRIPTION
+
+The module automatically setups an instance of Apache2 httpd server and destroys it when the perl script exits.
+
+=head1 FUNCTIONS
+
+=head2 new
+
+Creates and runs the httpd server.  Httpd is terminated when the returned object is DESTROYed.  The function accepts following arguments, which can also be read and/or be set through the accessors of the same name.
+
+=head3 listen
+
+The address to which the httpd binds to.  Corresponds to the "Listen" configuration directive of Apache.  The default value is "127.0.0.1:<whatever_port_that_was_unused>".
+
+=head3 server_root
+
+The "ServerRoot" runtime directive.  Set to current working directory if omitted.
+
+=head3 custom_conf
+
+Application-specific configuration passed that will be written to the configuration file of Apache.  Default is none.
+
+=head3 search_paths
+
+Paths to look for the httpd server in addition to the PATH environment variable.  The default is: /usr/sbin, /usr/local/sbin, /usr/local/apache/bin.
+
+=head3 pid
+
+the read-only accessor returns pid of the httpd server or undef if it is not running
+
+=head2 start
+
+the instance method starts the httpd server
+
+=head2 stop
+
+the instance method stops the httpd server
+
+=head1 COPYRIGHT
+
+Copyright (C) 2010 Cybozu Labs, Inc.  Written by Kazuho Oku.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+See L<http://www.perl.com/perl/misc/Artistic.html>
+
+=cut
