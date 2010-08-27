8f1e281bc053471b8e6e1bef3e7ce9456b13c036
Kazuho Oku <kazuhooku@gmail.com>
2010-08-27 12:34:46 +0900

    releng for 0.05

--- lib/Test/Httpd/Apache2.pm
+++ lib/Test/Httpd/Apache2.pm
@@ -16,7 +16,7 @@ use Time::HiRes qw(sleep);
 
 use constant PATH_SEP => $^O eq 'MSWin32' ? ';' : ':';
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 our %Defaults = (
     auto_start         => 1,
