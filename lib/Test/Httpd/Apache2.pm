289f375286d21f8a76ff71b1d3c0d0fcf838e749
Kazuho Oku <kazuhooku@gmail.com>
2010-09-01 20:21:39 +0900

    never call apxs on win32 (but use relative path from httpd)

--- lib/Test/Httpd/Apache2.pm
+++ lib/Test/Httpd/Apache2.pm
@@ -19,19 +19,18 @@ use constant PATH_SEP => $^O eq 'MSWin32' ? ';' : ':';
 our $VERSION = '0.07';
 
 our %Defaults = (
-    auto_start         => 1,
-    pid                => undef,
-    listen             => undef,
-    required_modules   => [],
-    server_root        => undef,
-    tmpdir             => undef,
-    custom_conf        => '',
-    search_paths       => [
+    auto_start       => 1,
+    pid              => undef,
+    listen           => undef,
+    required_modules => [],
+    server_root      => undef,
+    tmpdir           => undef,
+    custom_conf      => '',
+    search_paths     => [
         qw(/usr/sbin /usr/local/sbin /usr/local/apache/bin)
     ],
-    httpd              => 'httpd',
-    apxs               => 'apxs',
-    _fallback_dso_path => '',
+    httpd            => 'httpd',
+    apxs             => 'apxs',
 );
 
 if ($^O eq 'MSWin32') {
@@ -46,7 +45,8 @@ if ($^O eq 'MSWin32') {
         my $dso_path = $path;
         $dso_path =~ s!/bin$!/modules!;
         if (-d $dso_path) {
-            $Defaults{_fallback_dso_path} = $dso_path;
+            # convert to shortname since SPs in path will let the glob fail
+            $Defaults{_dso_path} = Win32::GetShortPathName($dso_path);
         }
     }
 } else {
@@ -237,14 +237,8 @@ sub get_dso_path {
             my $path;
             if (my $lines = $self->_read_cmd($self->apxs, '-q', 'LIBEXECDIR')) {
                 $path = (split /\n/, $lines)[0];
-            } elsif ($path = $self->_fallback_dso_path) {
-                warn "failed to obtain LIBEXECDIR from apxs, falling back to @{[$self->_fallback_dso_path]}";
             } else {
                 die "failed to determine the apache modules directory";
-            }
-            # convert to shortname since SPs in path will let the glob fail
-            if ($^O eq 'MSWin32') {
-                $path = Win32::GetShortPathName($path);
             }
             return $path;
         }->();
